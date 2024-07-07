5aa9ebafe41551f3b1037d309a6dc32ad9d3005d
raihanArman <raihanarman8@gmail.com>
2024-07-07 09:26:05 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Raihan Arman on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
